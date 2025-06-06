2e6ff76128283c4c15d9b7a301bdf13aabf93ac0
Konstatin Sazonov <moonug@gmail.com>
2025-06-06 12:43:09 +0300

    Return mysql57

--- Mk/Uses/mysql.mk
+++ Mk/Uses/mysql.mk
@@ -59,6 +59,7 @@ DEFAULT_MYSQL_VER?=	${MYSQL_DEFAULT:S/.//}
 # MySQL client version currently supported.
 # When adding a version, please keep the comment in
 # Mk/bsd.default-versions.mk in sync.
+MYSQL57_LIBVER=		20
 MYSQL80_LIBVER=		21
 MYSQL84_LIBVER=		24
 MYSQL91_LIBVER=		24
